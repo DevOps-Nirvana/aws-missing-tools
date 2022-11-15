fdcd8b6ce197cca56c4af7fdf5cc565df02c9d53
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2022-11-16 07:05:33 +1000

    detect run context and use fstrings (#10)
    Authored-by: Richtman, Ariel <ariel.richtman@silverrailtech.com>

--- cleanup-packer-aws-resources/cleanup-packer-aws-resources.py
+++ cleanup-packer-aws-resources/cleanup-packer-aws-resources.py
@@ -5,15 +5,12 @@
 #  cleanup-packer-aws-resources.py    Written by Farley <farley@neonsurge.com>
 #
 # Packer when used on an AWS account from tools like Jenkins or Rundeck, often
-# leaves reminants of its existance such as  instances running, security groups, 
-# and SSH keys.  This script scans all regions of AWS for leftover packer 
+# leaves remnants of its existance such as  instances running, security groups,
+# and SSH keys.  This script scans all regions of AWS for leftover packer
 # resources and removes them.
 #
 # This script can be run on the command-line standalone or ideally put into packer
 # and run via cloudwatch scheduled events like once a day or so
-#
-# NOTE: If you use this in AWS Lambda, please comment out the last line!  It's 
-#       not required but will make sure this script doesn't run an extra time
 #
 # This is from Farley's AWS missing tools
 #    https://github.com/DevOps-Nirvana/aws-missing-tools/
@@ -64,6 +61,8 @@ import boto3
 from pprint import pprint
 from datetime import datetime
 import calendar
+# For checking runtime context
+import os
 
 # The maximum age (in seconds) of a packer instance before we terminate it
 # 86400 == 1 day
@@ -80,7 +79,7 @@ ec2 = boto3.client('ec2', region_name='us-west-1')
 regions = []
 awsregions = ec2.describe_regions()['Regions']
 for region in awsregions:
-    regions.append(region['RegionName']) 
+    regions.append(region['RegionName'])
 del ec2, awsregions
 
 # Helper to convert datetime with TZ to Unix time
@@ -97,7 +96,7 @@ def display_time(seconds, granularity=2):
         ('minutes', 60),
         ('seconds', 1),
         )
-    
+
     result = []
 
     for name, count in intervals:
@@ -112,11 +111,11 @@ def display_time(seconds, granularity=2):
 # Get instances from AWS from all regions that...
 #    #1: Are currently running
 #    #2: Have the name "Packer Builder"
-#    #3: 
+#    #3: Have been alive longer than our specified limit
 def get_zombie_packer_instances(regions, maximum_age):
     global debug
     output = {}
-    
+
     # Get our "now" timestamp for knowing how long ago instances were launched
     utc_now = datetime.now()
     utc_now_ts  = int(utc_now.strftime("%s"))
@@ -124,7 +123,7 @@ def get_zombie_packer_instances(regions, maximum_age):
     for region in regions:
         regionoutput = []
         if debug is True:
-            print("Scanning region " + region + " for instances")
+            print(f"Scanning region {region} for instances")
 
         # Create our EC2 Handler
         ec2 = boto3.client('ec2', region_name=region)
@@ -135,9 +134,9 @@ def get_zombie_packer_instances(regions, maximum_age):
 
         for reservation in response['Reservations']:
             for instance in reservation['Instances']:
-                
+
                 if debug is True:
-                    print("Found instance: " + instance['InstanceId'])
+                    print(f"Found instance: {instance['InstanceId']}")
                 if instance['State']['Name'] == "running":
                     if debug is True:
                         print("  Instance is currently running")
@@ -145,7 +144,7 @@ def get_zombie_packer_instances(regions, maximum_age):
                     if debug is True:
                         print("  Instance is not currently running, skipping...")
                     continue
-                
+
                 packerNameTagMatched = False
                 if 'Tags' in instance:
                     for tag in instance['Tags']:
@@ -158,29 +157,29 @@ def get_zombie_packer_instances(regions, maximum_age):
                             if debug is True:
                                 print("  Instance is NOT a packer building, skipping...")
                             break
-                
+
                 if packerNameTagMatched is False:
                     continue
-                    
+
                 if debug is True:
-                    print("    Found packer instance: " + instance['InstanceId'])
+                    print(f"    Found packer instance: {instance['InstanceId']}")
                 launched_at = dt2ts(instance['LaunchTime'])
                 if debug is True:
-                    print("    Instance started " + display_time(utc_now_ts - launched_at) + " ago ")
+                    print(f"    Instance started {display_time(utc_now_ts - launched_at)} ago ")
                 # if (utc_now_ts - launched_at) > 86400:
                 if (utc_now_ts - launched_at) > maximum_age:
                     if debug is True:
                         print("    Instance started more than a day ago, should be marked for termination")
                     regionoutput.append({
-                        "region": region, 
+                        "region": region,
                         "instance_id": instance['InstanceId'],
                         "keyname": instance['KeyName'],
                         "security_groups": instance['SecurityGroups']
                     })
                 else:
                     if debug is True:
-                        print("    Instance is too new to be terminated")                    
-                    
+                        print("    Instance is too new to be terminated")
+
         output[region] = regionoutput
     return output
 
@@ -190,7 +189,7 @@ def get_zombie_packer_keys(regions):
     for region in regions:
         regionoutput = []
         if debug is True:
-            print("Scanning region " + region + " for keys")
+            print("Scanning region {region} for keys")
 
         # Create our EC2 Handler
         ec2 = boto3.client('ec2', region_name=region)
@@ -203,21 +202,21 @@ def get_zombie_packer_keys(regions):
                 },
             ]
         )
-        
+
         for pair in response['KeyPairs']:
             regionoutput.append(pair['KeyName'])
-        
+
         output[region] = regionoutput
     return output
-    
-    
+
+
 def get_zombie_packer_security_groups(regions):
     global debug
     output = {}
     for region in regions:
         regionoutput = []
         if debug is True:
-            print("Scanning region " + region + " for security groups")
+            print(f"Scanning region {region} for security groups")
 
         # Create our EC2 Handler
         ec2 = boto3.client('ec2', region_name=region)
@@ -226,49 +225,37 @@ def get_zombie_packer_security_groups(regions):
             Filters=[
                 {
                     'Name': 'group-name',
-                    'Values': ['packer *'],
+                    'Values': ['packer_*'],
                 },
             ]
         )
-            
+
         # NOTE:
         # Checking for stales doesn't seem to work, so we'll just try to delete without checking stale
-        # # Get our VPCs
-        # vpcs = ec2.describe_vpcs()['Vpcs']
-        # # Get all stale sgs in every vpc
-        # for vpc in vpcs:
-        #     stale = ec2.describe_stale_security_groups(VpcId=vpc['VpcId'])['StaleSecurityGroupSet']
-        #     pprint(stale)
-            
-        
         for pair in response['SecurityGroups']:
             regionoutput.append(pair['GroupName'])
-        
-        #if len(regionoutput) > 0:
-        #    stalegroups = ec2.describe_security_groups
-        #    for groupname in regionoutput:
-                
+
         output[region] = regionoutput
     return output
 
 def lambda_handler(event, context):
     global regions, max_age
-    
-    print("Scanning " + str(len(regions)) + " AWS regions for zombie packer instances...")
+
+    print(f"Scanning {len(regions)} AWS regions for zombie packer instances...")
 
     zombies = get_zombie_packer_instances(regions, max_age)
-    for region,instances in zombies.iteritems():
+    for region,instances in zombies.items():
         if len(instances) == 0:
-            print("Found NO zombie instances in " + region + ", skipping...")
+            print("Found NO zombie instances in {region}, skipping...")
             continue
 
-        print("Found " + str(len(instances)) + " zombie packer instances in " + region + ", now terminating...")
+        print(f"Found {len(instances)} zombie packer instances in {region}, now terminating...")
         ec2 = boto3.client('ec2', region_name=region)
-    
+
         instance_ids = []
         for instance in instances:
             instance_ids.append(instance['instance_id'])
-    
+
         try:
             response = ec2.terminate_instances(
                 InstanceIds=instance_ids
@@ -276,18 +263,18 @@ def lambda_handler(event, context):
             print("Successfully terminated instances")
         except:
             print("ERROR: Unable to terminate some or all resources")
-    
-    print("Scanning " + str(len(regions)) + " AWS regions for zombie packer keys...")
-    
+
+    print(f"Scanning {len(regions)} AWS regions for zombie packer keys...")
+
     zombies = get_zombie_packer_keys(regions)
-    for region,keynames in zombies.iteritems():
+    for region,keynames in zombies.items():
         if len(keynames) == 0:
-            print("Found NO zombie keys in " + region + ", skipping...")
+            print(f"Found NO zombie keys in {region}, skipping...")
             continue
-    
-        print("Found " + str(len(keynames)) + " zombie packer keys in " + region + ", now deleting...")
+
+        print(f"Found {len(keynames)} zombie packer keys in {region}, now deleting...")
         ec2 = boto3.client('ec2', region_name=region)
-    
+
         for keyname in keynames:
             try:
                 print("Deleting key " + keyname)
@@ -299,20 +286,20 @@ def lambda_handler(event, context):
                 print("Error while trying to terminate resources")
 
 
-    print("Scanning " + str(len(regions)) + " AWS regions for zombie packer security groups...")
+    print(f"Scanning {len(regions)} AWS regions for zombie packer security groups...")
 
     zombies = get_zombie_packer_security_groups(regions)
-    for region,security_groups in zombies.iteritems():
+    for region,security_groups in zombies.items():
         if len(security_groups) == 0:
             print("Found NO zombie security groups in " + region + ", skipping...")
             continue
-    
-        print("Found " + str(len(security_groups)) + " zombie security groups in " + region + ", now terminating...")
+
+        print(f"Found {len(security_groups)} zombie security groups in {region}, now terminating...")
         ec2 = boto3.client('ec2', region_name=region)
-    
+
         for security_group in security_groups:
             try:
-                print("Deleting security group " + security_group)
+                print(f"Deleting security group {security_group}")
                 response = ec2.delete_security_group(
                     GroupName=security_group
                 )
@@ -320,8 +307,11 @@ def lambda_handler(event, context):
             except:
                 print("Error while trying to terminate resources")
 
+# References:
+# https://unbiased-coder.com/detect-aws-env-python-nodejs/
+# https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
+def is_aws_env():
+    return os.environ.get('AWS_LAMBDA_FUNCTION_NAME') or os.environ.get('AWS_EXECUTION_ENV')
 
-# NOTE: REMOVE THIS BELOW FOR RUNNING ON LAMBDA otherwise spinning this lambda
-#       up will already run its main logic.  Which is okay, but not really intended
-#       If anyone knows how to detect running on Lambda please add this feature!  :P
-lambda_handler({}, "test")
+if not is_aws_env():
+    lambda_handler({}, {})
